265539ad5eeea68cbfc4cce8103439b212256940
mustali17 <74243780+mustali17@users.noreply.github.com>
2021-09-01 15:29:51 +0530

    update in about section

--- src/contents/About.js
+++ src/contents/About.js
@@ -12,11 +12,9 @@ class About extends Component {
             <br></br>
             <p>I started my journey in the world of computers from an young age,
             now I’m 18 years old, Pursuing my Software Engineering Degree in
-            Gandhinagar Institute of Technology.  Web development is my center of interest, i always
-            love the idea of cross-platform development, 1-n one code base deploy into almost
-            any platform, which web technology like Javascript enables me to do. I also like
+            Gandhinagar Institute of Technology.  Web development is my center of interest I also like
             creating Interactive UI components for better UX  and share those desgin and codes
-            to the world through Github, Codepen and Instagram.
+            to the world through Github.
             </p>
             
             </div>
