213861e4cdb93ce7e649a408bd0d5083a43ca478
Mustali <74243780+mustali17@users.noreply.github.com>
2021-09-02 09:06:40 +0530

    Add files via upload

---
+++ build/precache-manifest.0d4b3a1d4af80492152191ff59cb0e04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e050895c85c5f14a74df0cf61ab7d87",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e8f46e54d56cbb9cdacc",
+    "url": "/portfolio/static/css/main.2f9299cf.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/portfolio/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/portfolio/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8f46e54d56cbb9cdacc",
+    "url": "/portfolio/static/js/main.04b43bbe.chunk.js"
+  },
+  {
+    "revision": "72153f1981be4ee14a4e",
+    "url": "/portfolio/static/js/runtime-main.55975c6a.js"
+  },
+  {
+    "revision": "94830f0ea5c350e738f0bee93717f3f0",
+    "url": "/portfolio/static/media/mustali.94830f0e.jpg"
+  }
+]);
